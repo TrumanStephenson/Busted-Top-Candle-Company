b8d6570e143c07ee7e30da8af2af404c75ad3fc1
trumanstephenson <stephensontruman96@yahoo.com>
2024-08-15 21:21:31 -0800

    Making the primary main page. - Parallax scrolling - Bio/Mission     a. New info from owner - Gallery     a. Side scrolling gallery with js logic - Recording how many people are watching the site.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,40 @@
-var paramString = "";
-var searchParams = new URLSearchParams();
+const slideshow = document.querySelector('.slideshow');
+const slidesContainer = slideshow.querySelector('.slides');
+const slides = slidesContainer.querySelectorAll('.slide');
+const slideCount = slides.length;
+const slideWidth = 540;
 
-var urlArgsWhiteList = {
-    "beforeafter", "catid", "childforums",
+const highlightThumbnail = () => {
+  slideshow
+    .querySelectorAll('.highlighted')
+    .forEach(el => el.classList.remove('highlighted'));
+  const index = Math.floor(slidesContainer.scrollLeft / slideWidth);
+  slideshow
+    .querySelector(`.thumbnail[data-id="${index}"]`)
+    .classList.add('highlighted');
 };
+
+const scrollToElement = el => {
+  const index = parseInt(el.dataset.id, 10);
+  slidesContainer.scrollTo(index * slideWidth, 0);
+};
+
+// Create thumbnails
+const thumbnailsContainer = document.createElement('div');
+thumbnailsContainer.classList.add('thumbnails');
+slideshow.appendChild(thumbnailsContainer);
+
+slides.forEach((slide, i) => {
+  const thumbnail = document.createElement('div');
+  thumbnail.classList.add('thumbnail');
+  thumbnail.dataset.id = i;
+  thumbnailsContainer.appendChild(thumbnail);
+});
+
+thumbnailsContainer.querySelectorAll('.thumbnail').forEach(thumbnail => {
+  thumbnail.addEventListener('click', () => scrollToElement(thumbnail));
+});
+
+slidesContainer.addEventListener('scroll', highlightThumbnail);
+
+highlightThumbnail();
