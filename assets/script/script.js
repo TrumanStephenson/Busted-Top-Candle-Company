3f8bfccfec878b646d4a6319a5157009a6c703c3
TrumanStephenson <stephensontruman96@yahoo.com>
2025-01-07 08:02:48 -0900

    save state: getting ready to use enquire to make a change to the index class instead.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,53 +1,59 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-function registerMediaQueries(devices) {
-  Object.keys(devices).forEach(device => {
-    const width = devices[device].width;
-    const height = devices[device].height;
-
-    enquire.register('screen and (max-width: ' + width + 'px) and (max-height: ' + height + 'px)', {
-      match: () => {
-        console.log('Media query matched!');
-        handleResize();
-      },
-      unmatch: () => {
-        console.log('Media query unmatched!');
-        handleResize();
-      },
-    });
-  });
-}
-
-const devices = {
-  iPhoneSE: { width: 375, height: 667 },
-  iPhoneXR: { width: 414, height: 896 },
-  iPhone12Pro: { width: 390, height: 844 },
-  iPhone12ProMax: { width: 428, height: 926 },
-  iPad: { width: 768, height: 1024 },
-  iPadPro: { width: 1024, height: 1366 },
-  Pixel2: { width: 411, height: 731 },
-  Pixel2XL: { width: 411, height: 823 },
-  Pixel4: { width: 411, height: 869 },
-  Pixel4XL: { width: 411, height: 823 },
-  GalaxyS5: { width: 360, height: 640 },
-  GalaxyS9: { width: 360, height: 740 },
-  GalaxyS9Plus: { width: 360, height: 740 },
-  GalaxyNote9: { width: 360, height: 740 },
-  GalaxyFold: {
-    folded: { width: 280, height: 653 },
-    unfolded: { width: 653, height: 280 },
-  },
-  SurfaceDuo: {
-    singleScreen: { width: 540, height: 720 },
-    dualScreen: { width: 720, height: 540 },
-  },
-};
-
-registerMediaQueries(devices);
 
 
-// Come back to this later.
+
+// This failed. The bootstrap classes really helped clean up the size differences.
+// function registerMediaQueries(devices) {
+//   Object.keys(devices).forEach(device => {
+//     const width = devices[device].width;
+//     const height = devices[device].height;
+
+//     enquire.register('screen and (max-width: ' + width + 'px) and (max-height: ' + height + 'px)', {
+//       match: () => {
+//         console.log('Media query matched!');
+//         handleResize();
+//       },
+//       unmatch: () => {
+//         console.log('Media query unmatched!');
+//         handleResize();
+//       },
+//     });
+//   });
+// }
+
+// const devices = {
+//   iPhoneSE: { width: 375, height: 667 },
+//   iPhoneXR: { width: 414, height: 896 },
+//   iPhone12Pro: { width: 390, height: 844 },
+//   iPhone12ProMax: { width: 428, height: 926 },
+//   iPad: { width: 768, height: 1024 },
+//   iPadPro: { width: 1024, height: 1366 },
+//   Pixel2: { width: 411, height: 731 },
+//   Pixel2XL: { width: 411, height: 823 },
+//   Pixel4: { width: 411, height: 869 },
+//   Pixel4XL: { width: 411, height: 823 },
+//   GalaxyS5: { width: 360, height: 640 },
+//   GalaxyS9: { width: 360, height: 740 },
+//   GalaxyS9Plus: { width: 360, height: 740 },
+//   GalaxyNote9: { width: 360, height: 740 },
+//   GalaxyFold: {
+//     folded: { width: 280, height: 653 },
+//     unfolded: { width: 653, height: 280 },
+//   },
+//   SurfaceDuo: {
+//     singleScreen: { width: 540, height: 720 },
+//     dualScreen: { width: 720, height: 540 },
+//   },
+// };
+
+// registerMediaQueries(devices);
+
+
+//
+// Come back to this later. for the gallery.
+//
 // document.addEventListener('DOMContentLoaded', () => {
 //   const slideshow = document.querySelector('.slideshow');
 //   const slidesContainer = slideshow.querySelector('.slides');
