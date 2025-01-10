69c0315c03011e99fcb681dd947b59cf09e18312
TrumanStephenson <stephensontruman96@yahoo.com>
2025-01-10 11:24:05 -0900

    save state: This is crazy, I have no idea what I am doing right now, this enquire.js is so complicated. I think I might just revert to standard JS to handle this media query alteration. I mean it really does not work or override things as it should. I cannot believe this library still exists.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,40 +1,56 @@
-const devices = {
-      iPhoneSE: { width: 375, height: 667 },
-      iPhoneXR: { width: 414, height: 896 },
-      iPhone12Pro: { width: 390, height: 844 },
-      iPhone12ProMax: { width: 428, height: 926 },
-      iPad: { width: 768, height: 1024 },
-      iPadPro: { width: 1024, height: 1366 },
-      Pixel2: { width: 411, height: 731 },
-      Pixel2XL: { width: 411, height: 823 },
-      Pixel4: { width: 411, height: 869 },
-      Pixel4XL: { width: 411, height: 823 },
-      GalaxyS5: { width: 360, height: 640 },
-      GalaxyS9: { width: 360, height: 740 },
-      GalaxyS9Plus: { width: 360, height: 740 },
-      GalaxyNote9: { width: 360, height: 740 },
-      GalaxyFold: {
-        folded: { width: 280, height: 653 },
-        unfolded: { width: 653, height: 280 },
-      },
-      SurfaceDuo: {
-        singleScreen: { width: 540, height: 720 },
-        dualScreen: { width: 720, height: 540 },
-      },
-    };
+const devices = { 
+    GalaxyFold: { 
+        folded: { width: 280, height: 653 }, 
+        unfolded: { width: 653, height: 280 }, 
+    }, 
+    GalaxyS5: { width: 360, height: 640 }, 
+    GalaxyS9: { width: 360, height: 740 }, 
+    GalaxyS9Plus: { width: 360, height: 740 }, 
+    GalaxyNote9: { width: 360, height: 740 }, 
+    iPhoneSE: { width: 375, height: 667 }, 
+    iPhone12Pro: { width: 390, height: 844 }, 
+    Pixel2: { width: 411, height: 731 }, 
+    Pixel2XL: { width: 411, height: 823 }, 
+    Pixel4: { width: 411, height: 869 }, 
+    Pixel4XL: { width: 411, height: 823 }, 
+    iPhoneXR: { width: 414, height: 896 }, 
+    iPhone12ProMax: { width: 428, height: 926 }, 
+    SurfaceDuo: { 
+        singleScreen: { width: 540, height: 720 }, 
+        dualScreen: { width: 720, height: 540 }, 
+    }, 
+    iPad: { width: 768, height: 1024 }, 
+    iPadPro: { width: 1024, height: 1366 },
+};
 
-enquire.register("screen and (min-width: 1024px) and (max-width: 1280px)", {
-    match: () => {
-    document.querySelector(".toggle-img-2").classList.remove("d-none");
-    document.querySelector(".toggle-img-1").classList.add("d-none");
-    },
-});
-
-enquire.register("screen and (min-width: 1280px)", {
-    match: () => {
-    document.querySelector(".toggle-img-2").classList.add("d-none");
-    document.querySelector(".toggle-img-1").classList.remove("d-none");
-    },
+window.addEventListener('resize', function() {
+    let width = window.innerWidth;
+    
+    if (width <= devices.iPadPro.width) {
+        console.log('Screen size is less than iPad Pro');
+        enquire.register("screen and (min-width: 1024px) and (max-width: 1279px)", {
+            deferSetup: true,
+            setup: function() {
+                // This code will run only once, the first time the media query is matched 
+                console.log('Setup code executed'); 
+                // Example setup code: Initialize some elements or variables
+                const element = document.querySelector(".toggle-img-2"); 
+                if (element) { 
+                    element.style.backgroundColor = "blue"; 
+                }
+            }, match: function() {
+                    document.querySelector(".toggle-img-2").classList.remove("d-none");
+                    document.querySelector(".toggle-img-1").classList.add("d-none"); },
+                unmatch: function() {
+                    document.querySelector(".toggle-img-2").classList.add("d-none");
+                    document.querySelector(".toggle-img-1").classList.remove("d-none");
+            }, destroy: function() {
+                console.log('Handler unregistered'); } });
+        console.log(enquire.register());
+    } else if (width >= 600 && width < 1200) {
+        console.log('Screen size is between 600px and 1200px');
+        
+    }
 });
 
 //
