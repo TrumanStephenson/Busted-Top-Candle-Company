496501b10a6deb9835427073b4d33d8e30ad6636
TrumanStephenson <stpehensontruman96@yahoo.com>
2025-12-16 21:34:19 -0800

    save state: - Fixed margin and padding issues in index.html - Removed unused CSS rules in style.css - .script file is basically useless. Everything     is handled by Bootstrap - Removed unusued alt tags in index.html - Footer contact info updated - Removed css file for bootstrap5

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,43 +1,43 @@
-const devices = { 
-    GalaxyFold: { 
-        folded: { width: 280, height: 653 }, 
-        unfolded: { width: 653, height: 280 }, 
-    }, 
-    GalaxyS5: { width: 360, height: 640 }, 
-    GalaxyS9: { width: 360, height: 740 }, 
-    GalaxyS9Plus: { width: 360, height: 740 }, 
-    GalaxyNote9: { width: 360, height: 740 }, 
-    iPhoneSE: { width: 375, height: 667 }, 
-    iPhone12Pro: { width: 390, height: 844 }, 
-    Pixel2: { width: 411, height: 731 }, 
-    Pixel2XL: { width: 411, height: 823 }, 
-    Pixel4: { width: 411, height: 869 }, 
-    Pixel4XL: { width: 411, height: 823 }, 
-    iPhoneXR: { width: 414, height: 896 }, 
-    iPhone12ProMax: { width: 428, height: 926 }, 
-    SurfaceDuo: { 
-        singleScreen: { width: 540, height: 720 }, 
-        dualScreen: { width: 720, height: 540 }, 
-    }, 
-    iPad: { width: 768, height: 1024 }, 
-    iPadPro: { width: 1024, height: 1366 },
-};
+// const devices = { 
+//     GalaxyFold: { 
+//         folded: { width: 280, height: 653 }, 
+//         unfolded: { width: 653, height: 280 }, 
+//     }, 
+//     GalaxyS5: { width: 360, height: 640 }, 
+//     GalaxyS9: { width: 360, height: 740 }, 
+//     GalaxyS9Plus: { width: 360, height: 740 }, 
+//     GalaxyNote9: { width: 360, height: 740 }, 
+//     iPhoneSE: { width: 375, height: 667 }, 
+//     iPhone12Pro: { width: 390, height: 844 }, 
+//     Pixel2: { width: 411, height: 731 }, 
+//     Pixel2XL: { width: 411, height: 823 }, 
+//     Pixel4: { width: 411, height: 869 }, 
+//     Pixel4XL: { width: 411, height: 823 }, 
+//     iPhoneXR: { width: 414, height: 896 }, 
+//     iPhone12ProMax: { width: 428, height: 926 }, 
+//     SurfaceDuo: { 
+//         singleScreen: { width: 540, height: 720 }, 
+//         dualScreen: { width: 720, height: 540 }, 
+//     }, 
+//     iPad: { width: 768, height: 1024 }, 
+//     iPadPro: { width: 1024, height: 1366 },
+// };
 
-// Handling the resize
-window.addEventListener('resize' && 'load', function() {
-    let width = window.innerWidth;
-    const toggledImg1 = document.querySelectorAll(".toggle-img-1");
-    const toggledImg2 = document.querySelectorAll(".toggle-img-2");
+// // Handling the resize
+// window.addEventListener('resize' && 'load', function() {
+//     let width = window.innerWidth;
+//     const toggledImg1 = document.querySelectorAll(".toggle-img-1");
+//     const toggledImg2 = document.querySelectorAll(".toggle-img-2");
 
-    if (width <= devices.iPadPro.width) {
-        toggledImg1.forEach(img => img.style.display = 'none');
-        toggledImg2.forEach(img => img.style.display = 'block');
-    } else {
-        toggledImg1.forEach(img => img.style.display = 'block');
-        toggledImg2.forEach(img => img.style.display = 'none');
-        console.log(toggledImg1, toggledImg2);
-    }
-});
+//     if (width <= devices.iPadPro.width) {
+//         toggledImg1.forEach(img => img.style.display = 'none');
+//         toggledImg2.forEach(img => img.style.display = 'block');
+//     } else {
+//         toggledImg1.forEach(img => img.style.display = 'block');
+//         toggledImg2.forEach(img => img.style.display = 'none');
+//         console.log(toggledImg1, toggledImg2);
+//     }
+// });
 
 //
 // Come back to this later. for the gallery.
