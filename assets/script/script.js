c844f166db721ac673c482a63f86d0671794b0bc
TrumanStephenson <stephensontruman96@yahoo.com>
2025-12-29 16:36:34 -0800

    save state: - added forms page for order submissions - started some script to reduce amount of html:     this reduced the amount of options in the form     select menus. - adjusted styles for better appearance - created shortcut links in the header.     this need refinement. - cleaned up the padding issue on the side that     added a vertical bar to the side of the page.

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+const states = [ 
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", 
+    "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", 
+    "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", 
+    "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY" 
+];
+
+const select = document.getElementById("inputState");
+
+states.forEach(state => {
+    const option = document.createElement("option");
+    option.textContent = state;
+    option.value = state;
+    select.appendChild(option);
+});
